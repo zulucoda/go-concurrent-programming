8de54bc481adb7027fbd55807dd475ac8bdf2afa
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-07-31 22:59:32 +0200

    updating to use mutex to handle race conditions

--- main.go
+++ main.go
@@ -18,28 +18,32 @@ func main() {
 	// pointer.
 	wg := &sync.WaitGroup{}
 
+	// mutex is similar to a waitGroup, used to handle race conditions
+	// Note: you dont want to copy a mutex as you pass it around. Therefore use a pointer
+	mutex := &sync.Mutex{}
+
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 
 		// This lets the waitGroup know that there 2 task that we are waiting on
 		wg.Add(2)
 
-		go func(id int, wg *sync.WaitGroup) {
-			if c, ok := queryCache(id); ok {
+		go func(id int, wg *sync.WaitGroup, mutex *sync.Mutex) {
+			if c, ok := queryCache(id, mutex); ok {
 				fmt.Println("From cache")
 				fmt.Println(c)
 			}
 			// When the task is done, we let the waitGroup know
 			wg.Done()
-		}(id, wg)
-		go func(id int, wg *sync.WaitGroup) {
-			if c, ok := queryDatabase(id); ok {
+		}(id, wg, mutex)
+		go func(id int, wg *sync.WaitGroup, mutex *sync.Mutex) {
+			if c, ok := queryDatabase(id, mutex); ok {
 				fmt.Println("from database")
 				fmt.Println(c)
 			}
 			// When the task is done, we let the waitGroup know
 			wg.Done()
-		}(id, wg)
+		}(id, wg, mutex)
 		//fmt.Printf("Car not found with id: '%v'", id)
 		//time.Sleep(150 * time.Millisecond)
 	}
@@ -49,16 +53,23 @@ func main() {
 
 }
 
-func queryCache(id int) (Car, bool) {
+func queryCache(id int, mutex *sync.Mutex) (Car, bool) {
+
+	// whichever goroutine called this, now owns the mutex. So nothing else is going to be able
+	// to access procteded code until the owing goroutine calls UnLock
+	mutex.Lock()
 	c, ok := cache[id]
+	mutex.Unlock()
 	return c, ok
 }
 
-func queryDatabase(id int) (Car, bool) {
+func queryDatabase(id int, mutex *sync.Mutex) (Car, bool) {
 	time.Sleep(100 * time.Millisecond) // mock DB
 	for _, c := range cars {
 		if c.ID == id {
-			//	cache[id] = c
+			mutex.Lock()
+			cache[id] = c
+			mutex.Unlock()
 			return c, true
 		}
 	}
