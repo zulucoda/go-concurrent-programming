535cf630249dfc17dc7b8d5ffd18cd12994cd011
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-07-31 23:37:13 +0200

    channel types - receive only and send only

---
+++ channel-types/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	fmt.Println("vim-go")
+
+	wg := &sync.WaitGroup{}
+
+	// creating a buffered channel with 1 message
+	ch := make(chan int, 1)
+
+	wg.Add(2)
+
+	// receiving channel
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		fmt.Println(<-ch)
+		wg.Done()
+	}(ch, wg)
+
+	// send channel
+	go func(ch chan<- int, wg *sync.WaitGroup) {
+		ch <- 42
+		wg.Done()
+
+	}(ch, wg)
+	wg.Wait()
+}
