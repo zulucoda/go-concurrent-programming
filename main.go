5596c1ee43458f2e361e6a65504233e68dfb4b34
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-07-31 23:05:54 +0200

    using RW mutex to allow for many goroutines to read from cache

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 
 	// mutex is similar to a waitGroup, used to handle race conditions
 	// Note: you dont want to copy a mutex as you pass it around. Therefore use a pointer
-	mutex := &sync.Mutex{}
+	mutex := &sync.RWMutex{}
 
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
@@ -28,7 +28,7 @@ func main() {
 		// This lets the waitGroup know that there 2 task that we are waiting on
 		wg.Add(2)
 
-		go func(id int, wg *sync.WaitGroup, mutex *sync.Mutex) {
+		go func(id int, wg *sync.WaitGroup, mutex *sync.RWMutex) {
 			if c, ok := queryCache(id, mutex); ok {
 				fmt.Println("From cache")
 				fmt.Println(c)
@@ -36,7 +36,7 @@ func main() {
 			// When the task is done, we let the waitGroup know
 			wg.Done()
 		}(id, wg, mutex)
-		go func(id int, wg *sync.WaitGroup, mutex *sync.Mutex) {
+		go func(id int, wg *sync.WaitGroup, mutex *sync.RWMutex) {
 			if c, ok := queryDatabase(id, mutex); ok {
 				fmt.Println("from database")
 				fmt.Println(c)
@@ -53,20 +53,22 @@ func main() {
 
 }
 
-func queryCache(id int, mutex *sync.Mutex) (Car, bool) {
+func queryCache(id int, mutex *sync.RWMutex) (Car, bool) {
 
-	// whichever goroutine called this, now owns the mutex. So nothing else is going to be able
-	// to access procteded code until the owing goroutine calls UnLock
-	mutex.Lock()
+	// This allow mutliple goroutines to read from the cache
+	mutex.RLock()
 	c, ok := cache[id]
-	mutex.Unlock()
+	mutex.RUnlock()
 	return c, ok
 }
 
-func queryDatabase(id int, mutex *sync.Mutex) (Car, bool) {
+func queryDatabase(id int, mutex *sync.RWMutex) (Car, bool) {
 	time.Sleep(100 * time.Millisecond) // mock DB
 	for _, c := range cars {
 		if c.ID == id {
+
+			// whichever goroutine called this, now owns the mutex. So nothing else is going to be able
+			// to access procteded code until the owing goroutine calls UnLock
 			mutex.Lock()
 			cache[id] = c
 			mutex.Unlock()
